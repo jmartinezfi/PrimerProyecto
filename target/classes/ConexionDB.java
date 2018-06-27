ee7e371c168dc82422c272756e0cb95546721a09
jmartinezfi <jucamafi09@gmail.com>
2018-06-27 11:58:37 -0500

    v270620181158

--- target/classes/ConexionDB.java
+++ target/classes/ConexionDB.java
@@ -18,9 +18,59 @@ public class ConexionDB {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 		} catch (Exception e) {
-
 		}
 		return conn;
 	}
-
+	public int getSecuencia(String nombresq) {
+		int secuencia = 0;
+		java.sql.Connection conn = null;
+		java.sql.PreparedStatement pstm  = null;
+		java.sql.ResultSet rs = null;
+		try {
+			int codigo = 0;
+			conn = getConexion();
+			String sql = "Select * from sistema_secuencias " ;
+			sql += " where TABLA =  ? ";
+			pstm = conn.prepareStatement(sql);
+			pstm.setString(1, nombresq);
+			rs = pstm.executeQuery();
+			while(rs.next()){
+				secuencia = rs.getInt("secuencia");
+				codigo = rs.getInt("ID_SECUENCIA");
+			}
+			pstm.close();
+			secuencia ++;
+			sql = "update sistema_secuencias set " ;
+			sql += " secuencia = ? ";
+			sql += " where  ID_SECUENCIA = ? " ; 
+			pstm = conn.prepareStatement(sql);
+			pstm.setInt(1, secuencia);
+			pstm.setInt(2, codigo);
+			pstm.execute();
+		} catch (java.sql.SQLException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (java.sql.SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (pstm != null)
+					pstm.close();
+			} catch (java.sql.SQLException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (conn != null)
+					conn.close();
+			} catch (java.sql.SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return secuencia;
+	}
 }
